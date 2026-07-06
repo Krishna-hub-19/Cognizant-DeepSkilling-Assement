ea363f9dd5b08a537c633b40c27f356da15ba2e5
Krishna Garg <garg62455@gmail.com>
2026-07-06 16:46:00 +0530

    Exercise 4: Create CountryRepository and CountryService

---
+++ Spring Data JPA/orm-learn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,36 @@
+package com.cognizant.ormlearn.service;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    public Optional<Country> getCountry(String code) {
+        return countryRepository.findById(code);
+    }
+
+    public Country addCountry(Country country) {
+        return countryRepository.save(country);
+    }
+
+    public Country updateCountry(Country country) {
+        return countryRepository.save(country);
+    }
+
+    public void deleteCountry(String code) {
+        countryRepository.deleteById(code);
+    }
+}
