35e1db69416ee6d6ca662a81fbc0d8b2d7e30520
Krishna Garg <garg62455@gmail.com>
2026-07-10 21:50:53 +0530

    exercise 2: implement HQL query and optimize using join fetch

---
+++ Spring Data JPA with Hibernate/handson-3/orm-learn/src/main/java/com/cognizant/ormlearn/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package com.cognizant.ormlearn.service;
+
+import com.cognizant.ormlearn.model.Employee;
+import com.cognizant.ormlearn.repository.EmployeeRepository;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(EmployeeService.class);
+
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public Employee get(int id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    @Transactional
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+
+    @Transactional
+    public List<Employee> getAllPermanentEmployees() {
+
+        LOGGER.info("Start");
+
+        List<Employee> employees = employeeRepository.getAllPermanentEmployees();
+
+        LOGGER.info("End");
+
+        return employees;
+    }
+}
