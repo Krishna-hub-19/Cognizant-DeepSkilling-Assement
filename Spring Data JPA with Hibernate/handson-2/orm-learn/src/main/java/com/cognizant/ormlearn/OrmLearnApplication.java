f4182e85072259c979705d9cedea8c97be3a8697
Krishna Garg <garg62455@gmail.com>
2026-07-10 14:36:16 +0530

    Exercise - 4: Getting Employee along with Department

--- Spring Data JPA with Hibernate/handson-2/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
+++ Spring Data JPA with Hibernate/handson-2/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -1,33 +1,55 @@
 package com.cognizant.ormlearn;
 
-import com.cognizant.ormlearn.model.Stock;
-import com.cognizant.ormlearn.service.StockService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.cognizant.ormlearn.model.Employee;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.time.LocalDate;
+import com.cognizant.ormlearn.service.DepartmentService;
+import com.cognizant.ormlearn.service.EmployeeService;
+import com.cognizant.ormlearn.service.SkillService;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class OrmLearnApplication implements CommandLineRunner {
 
-    private final StockService stockService;
+    private static EmployeeService employeeService;
+    private static DepartmentService departmentService;
+    private static SkillService skillService;
 
-    public OrmLearnApplication(StockService stockService) {
-        this.stockService = stockService;
-    }
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(OrmLearnApplication.class);
+
 
 
     public static void main(String[] args) {
-        SpringApplication.run(OrmLearnApplication.class, args);
+
+        ConfigurableApplicationContext context =
+                SpringApplication.run(OrmLearnApplication.class, args);
+
+        employeeService = context.getBean(EmployeeService.class);
+        departmentService = context.getBean(DepartmentService.class);
+        skillService = context.getBean(SkillService.class);
+
+        testGetEmployee();
     }
 
     @Override
     public void run(String... args) {
+        // Exercise 2 code commented out
+    }
 
-        stockService
-                .getLowestNetflixStocks()
-                .forEach(System.out::println);
+    private static void testGetEmployee() {
 
+        LOGGER.info("Start");
+
+        Employee employee = employeeService.get(1);
+
+        LOGGER.info("Employee: {}", employee);
+
+        LOGGER.info("Department: {}", employee.getDepartment());
+
+        LOGGER.info("End");
     }
 }
