1c30ef4c209a49e6572198f97103c4af107d5128
Krishna Garg <garg62455@gmail.com>
2026-07-14 22:59:22 +0530

    exercise-6: implement Proxy pattern for image viewer with lazy loading

---
+++ Enginnering concepts - Data Structures and design pattern/Design_Patterns_and_Principles/DesignPatternsAndPrinciples/src/main/java/com/cognizant/proxy/ProxyPatternTest.java
@@ -0,0 +1,27 @@
+package com.cognizant.proxy;
+
+public class ProxyPatternTest {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("nature.jpg");
+
+        System.out.println("First Display");
+
+        image.display();
+
+        System.out.println();
+
+        System.out.println("Second Display");
+
+        image.display();
+
+        System.out.println();
+
+        System.out.println("Third Display");
+
+        image.display();
+
+    }
+
+}
