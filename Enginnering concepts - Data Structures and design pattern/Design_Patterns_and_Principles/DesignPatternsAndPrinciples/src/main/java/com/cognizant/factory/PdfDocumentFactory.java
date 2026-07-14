4c7ee230bef07b1e215077302c4c75f9a0ccec17
Krishna Garg <garg62455@gmail.com>
2026-07-14 22:29:52 +0530

    exercise-2: implement Factory Method pattern for document management system

---
+++ Enginnering concepts - Data Structures and design pattern/Design_Patterns_and_Principles/DesignPatternsAndPrinciples/src/main/java/com/cognizant/factory/PdfDocumentFactory.java
@@ -0,0 +1,10 @@
+package com.cognizant.factory;
+
+public class PdfDocumentFactory extends DocumentFactory {
+
+    @Override
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+
+}
