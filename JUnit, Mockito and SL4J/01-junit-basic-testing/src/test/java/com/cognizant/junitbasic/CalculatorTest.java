5587af700c1ecb27be5f3d4cd5e61a92de915381
Krishna Garg <garg62455@gmail.com>
2026-07-15 22:29:10 +0530

    JUnit_Basic Testing - exercise 2: Add Calculator class and basic JUnit test cases

---
+++ JUnit, Mockito and SL4J/01-junit-basic-testing/src/test/java/com/cognizant/junitbasic/CalculatorTest.java
@@ -0,0 +1,31 @@
+package com.cognizant.junitbasic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAddition() {
+        assertEquals(10, calculator.add(5,5));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(2, calculator.subtract(5,3));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(15, calculator.multiply(3,5));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(2, calculator.divide(10,5));
+    }
+
+}
