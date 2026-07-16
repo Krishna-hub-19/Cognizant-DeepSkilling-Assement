c095f461fbec6bd8a253d5a86b7e79c9bd8f74d6
Krishna Garg <garg62455@gmail.com>
2026-07-16 18:05:02 +0530

    Mockito_exercises - exercise 2: Verify mock interactions using Mockito verify()

--- JUnit, Mockito and SL4J/03-mockito-exercises/src/test/java/com/cognizant/mockito/MyServiceTest.java
+++ JUnit, Mockito and SL4J/03-mockito-exercises/src/test/java/com/cognizant/mockito/MyServiceTest.java
@@ -12,9 +12,7 @@ public class MyServiceTest {
 
         // Arrange
         ExternalApi mockApi = mock(ExternalApi.class);
-
-        when(mockApi.getData())
-                .thenReturn("Mock Data");
+        when(mockApi.getData()).thenReturn("Mock Data");
 
         MyService service = new MyService(mockApi);
 
@@ -23,5 +21,36 @@ public class MyServiceTest {
 
         // Assert
         assertEquals("Mock Data", result);
+    }
+
+    @Test
+    void testVerifyInteraction() {
+
+        // Arrange
+        ExternalApi mockApi = mock(ExternalApi.class);
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service = new MyService(mockApi);
+
+        // Act
+        service.fetchData();
+
+        // Assert
+        verify(mockApi).getData();
+    }
+
+    @Test
+    void testVerifyTwoCalls() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+        service.fetchData();
+
+        verify(mockApi, times(2)).getData();
     }
 }
