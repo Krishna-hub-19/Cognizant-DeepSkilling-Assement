e3b16dcf4dd2ae8fab9dafa2cd19f2414f7b77de
Krishna Garg <garg62455@gmail.com>
2026-07-17 16:58:27 +0530

    Mockito_Mock dependencies - exercise 3: Mock UserService in integration test using @SpringBootTest

---
+++ JUnit, Mockito and SL4J/06-Mockito_Mock dependencies/src/test/java/com/cognizant/springmockito/UserIntegrationTest.java
@@ -0,0 +1,43 @@
+package com.cognizant.springmockito;
+
+import com.cognizant.springmockito.entity.User;
+import com.cognizant.springmockito.service.UserService;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    void testGetUser() throws Exception {
+
+        User user = new User(1L, "Krishna");
+
+        when(userService.getUserById(1L))
+                .thenReturn(user);
+
+        mockMvc.perform(get("/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Krishna"));
+
+    }
+
+}
