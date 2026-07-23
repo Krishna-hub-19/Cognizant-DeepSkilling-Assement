cbeca482a615864efd5e09b61ab34b7cae77a62e
Krishna Garg <garg62455@gmail.com>
2026-07-23 12:51:17 +0530

    1. spring-rest-handson - exercise 3: Add SLF4J logging to Spring Boot application

--- Spring REST using Spring Boot/1. spring-rest-handson/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Spring REST using Spring Boot/1. spring-rest-handson/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,5 +1,7 @@
-package com.cognizant.springlearn;
+package com.cognizant.spring_learn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +13,9 @@ import java.util.Date;
 @SpringBootApplication
 public class SpringLearnApplication {
 
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
     public static void main(String[] args) {
 
         SpringApplication.run(SpringLearnApplication.class, args);
@@ -19,6 +24,8 @@ public class SpringLearnApplication {
     }
 
     public static void displayDate() {
+
+        LOGGER.info("START");
 
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("date-format.xml");
@@ -30,12 +37,14 @@ public class SpringLearnApplication {
 
             Date date = format.parse("31/12/2018");
 
-            System.out.println(date);
+            LOGGER.debug("Date : {}", date);
 
         } catch (Exception e) {
 
-            e.printStackTrace();
+            LOGGER.error("Exception occurred while parsing date", e);
 
         }
+
+        LOGGER.info("END");
     }
 }
