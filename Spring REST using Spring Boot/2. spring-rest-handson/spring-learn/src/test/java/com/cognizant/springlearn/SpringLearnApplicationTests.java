2b4712e65340d86a148fd5400e2a996852499d98
Krishna Garg <garg62455@gmail.com>
2026-07-23 14:26:17 +0530

    2. spring-rest-handson - exercise 6: Test get country service using MockMVC

--- Spring REST using Spring Boot/2. spring-rest-handson/spring-learn/src/test/java/com/cognizant/springlearn/SpringLearnApplicationTests.java
+++ Spring REST using Spring Boot/2. spring-rest-handson/spring-learn/src/test/java/com/cognizant/springlearn/SpringLearnApplicationTests.java
@@ -1,13 +1,48 @@
 package com.cognizant.springlearn;
 
+import com.cognizant.springlearn.controller.CountryController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.junit.jupiter.api.Test;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringLearnApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private CountryController countryController;
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(countryController);
+    }
+
+    @Test
+    void testGetCountry() throws Exception {
+
+        ResultActions actions =
+                mvc.perform(get("/country"));
+
+        actions.andExpect(status().isOk());
+
+        actions.andExpect(jsonPath("$.code").exists());
+
+        actions.andExpect(jsonPath("$.code").value("IN"));
+
+        actions.andExpect(jsonPath("$.name").exists());
+
+        actions.andExpect(jsonPath("$.name").value("India"));
+    }
 }
