d3c2a043676455d2b2ed59fb3600cf79698a1f29
Krishna Garg <garg62455@gmail.com>
2026-07-23 14:03:48 +0530

    2. spring-rest-handson - exercise 4: Implement get country by code REST service

--- Spring REST using Spring Boot/2. spring-rest-handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Spring REST using Spring Boot/2. spring-rest-handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -1,11 +1,14 @@
 package com.cognizant.springlearn.controller;
 
 import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +17,9 @@ import java.util.List;
 
 @RestController
 public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
 
     private static final Logger LOGGER =
             LoggerFactory.getLogger(CountryController.class);
@@ -54,5 +60,19 @@ public class CountryController {
         LOGGER.info("END");
 
         return countryList;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        LOGGER.info("START");
+
+        Country country = countryService.getCountry(code);
+
+        LOGGER.debug("Country : {}", country);
+
+        LOGGER.info("END");
+
+        return country;
     }
 }
---
+++ Spring REST using Spring Boot/2. spring-rest-handson/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -0,0 +1,33 @@
+package com.cognizant.springlearn.service;
+
+import com.cognizant.springlearn.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    @SuppressWarnings("unchecked")
+    public Country getCountry(String code) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countryList =
+                context.getBean("countryList", ArrayList.class);
+
+        for (Country country : countryList) {
+
+            if (country.getCode().equalsIgnoreCase(code)) {
+                return country;
+            }
+
+        }
+
+        return null;
+    }
+}
